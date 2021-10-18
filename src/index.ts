a976f2ffa472c1d3e74edcdac7a2e4ae0f841872
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-18 09:48:55 +0900

    feat: loader 를 이용하여 app listen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hellworld");
+import express from "express";
+import loader from "./loader";
+
+const app = express();
+
+const loadSettings = async () => {
+  await loader({ expressApp: app });
+};
+
+loadSettings();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
