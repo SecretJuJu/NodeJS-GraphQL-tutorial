54cf759d49a46f545042a3dcf3ab2bd814a37eca
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-18 14:16:44 +0900

    feat: loader에서 mongo 커넥션 만들기

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 
 import expressLoader from "./express";
+import connectDB from "config/connect-mongo";
+
+connectDB();
 
 export interface LoaderSetting {
   expressApp: express.Express;
