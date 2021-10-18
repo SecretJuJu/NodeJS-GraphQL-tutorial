d13592551987b3ee4ef74cbfb9e741f6b6964e73
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-18 13:17:36 +0900

    add: auth controller 추가

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const login = (req: express.Request, res: express.Response) => {};
+const register = (req: express.Request, res: express.Response) => {};
+
+const AuthController = {
+  login,
+  register,
+};
+
+export default AuthController;
