c91d8c0265bf2f74224015e95fdc1d9da99f8cb2
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-19 11:18:56 +0900

    upd: create user conflict error 핸들링

--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -1,24 +1,37 @@
 import { Model } from "mongoose";
+import * as bcrypt from "bcrypt";
 
-import { User, UserModelDocument } from "models/user.model";
+import { User, UserModelDocument, UserObject } from "models/user.model";
 import { CreateUserDto } from "./dtos/create-user.dto";
+import { CONFLICT_ERROR } from "error";
 
 export class UserService {
   constructor(private User: Model<UserModelDocument>) {}
 
-  async getUserByUsername(username: string) {
-    console.log("Search with", username);
-    const user = await this.User.findOne({ username });
-    if (!user) {
-      return null;
-    } else {
-      return user;
-    }
+  async getUserByUsername(username: string): Promise<UserModelDocument | null> {
+    const user: UserModelDocument | null = await this.User.findOne({
+      username,
+    });
+
+    return user;
   }
 
   async createUser(createUserDto: CreateUserDto) {
-    const newUser = await this.User.create(createUserDto);
-    console.log(newUser);
+    const isSameUserExist: boolean =
+      (await this.getUserByUsername(createUserDto.username)) === null
+        ? false
+        : true;
+
+    if (!isSameUserExist) {
+      // conflict error
+      throw new CONFLICT_ERROR();
+    }
+
+    const hashedPassword = bcrypt.hashSync(createUserDto.password, 10); // sync
+    createUserDto.password = hashedPassword;
+    const newUser: UserObject = await this.User.create(createUserDto);
+    newUser.save();
+    return newUser;
   }
 }
 
