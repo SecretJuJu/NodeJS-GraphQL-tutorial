709cf9a373191018e502e97b8c91cde8dd404f3a
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-10-19 21:44:13 +0900

    feat: login 기능 추가

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 import RequestValidator from "utils/request-validator";
 import CreateUserRequest from "request/auth/create-user.request";
 import { CreateUserDto } from "service/user/dtos/create-user.dto";
-import authService, { AuthService } from "service/auth/auth.service";
+import authService from "service/auth/auth.service";
+import LoginRequest from "request/auth/login.request";
+import jwtService from "service/jwt/jwt.service";
 
 class AuthController {
   constructor() {}
 
-  login(req: Request, res: Response) {}
+  @RequestValidator(LoginRequest)
+  async login(req: LoginRequest, res: Response) {
+    const { username, password } = req;
+    const user = await authService.validateUser(username, password);
+    const token = jwtService.createAccessToken(user);
+    return res.status(StatusCodes.OK).json({ token });
+  }
 
   @RequestValidator(CreateUserRequest)
   async register(req: CreateUserRequest, res: Response) {
